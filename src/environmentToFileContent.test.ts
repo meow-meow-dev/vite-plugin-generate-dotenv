83984be410ff41e570239bb0818a2ba0373147fd
Jérôme <jfreyheit@gmail.com>
2025-01-10 18:17:21 +0700

    feat: ability to add a multi-line generated notice

--- src/environmentToFileContent.test.ts
+++ src/environmentToFileContent.test.ts
@@ -9,9 +9,20 @@ describe("environmentToFileContent", () => {
     );
   });
 
-  it("adds the header", ({ expect }) => {
+  it("adds a single-line header", ({ expect }) => {
     expect(
       environmentToFileContent({ A: "a", B: "b" }, "# Auto-generated !"),
     ).toEqual('# Auto-generated !\n\nA="a"\nB="b"\n');
+  });
+
+  it("adds a multi-lines header", ({ expect }) => {
+    expect(
+      environmentToFileContent({ A: "a", B: "b" }, [
+        "# Auto-generated",
+        "Don't touch or you'll be fired",
+      ]),
+    ).toEqual(
+      '# Auto-generated\nDon\'t touch or you\'ll be fired\n\nA="a"\nB="b"\n',
+    );
   });
 });
--- src/environmentToFileContent.ts
+++ src/environmentToFileContent.ts
@@ -1,11 +1,18 @@
 export function environmentToFileContent(
   environment: Record<string, string>,
-  header?: string,
+  header?: string | string[],
 ): string {
   const lines = Object.entries(environment).map(
     ([key, value]) => `${key}="${value}"`,
   );
 
-  const headerLines = header === undefined ? [] : [header, ""];
-  return [...headerLines, ...lines, "" /* add a final line feed */].join("\n");
+  return [...normalize(header), ...lines, "" /* add a final line feed */].join(
+    "\n",
+  );
+}
+
+function normalize(lines: string | string[] | undefined): string[] {
+  if (Array.isArray(lines)) return [...lines, ""];
+  else if (typeof lines === "string") return [lines, ""];
+  else return [];
 }
